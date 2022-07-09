889a178d1075f2ea4d73789b9a04228b393ed3cb
HuynhVong <71302943+HuynhVong@users.noreply.github.com>
2022-07-09 16:30:37 +0700

    To newsltter Subsriber
    to be continued

---
+++ responsive-webui/src/components/Projects.js
@@ -0,0 +1,81 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Flaner",
+            description: "Social Network on mobile design",
+            imgUrl: projImg1
+        },
+        {
+            title: "ComeBuy",
+            description: "A buying laptop website",
+            imgUrl: projImg2
+        },
+        {
+            title: "Coolzy",
+            description: "A clothes shop online",
+            imgUrl: projImg3
+        },
+        {
+            title: "GoTour",
+            description: "A travel application on mobile",
+            imgUrl: projImg2
+        },
+    ];
+
+    return (
+        <section className="project" id="projects">
+            <Container>
+                <Row>
+                    <Col size={12}>
+                        <h2>Projects</h2>
+                        <p>I had been working on these collaborated projects so as to improve, practice and even studying my software developing skills. Moreover, these helped me submit my assignments at school over 3 years along works happen</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <ProjectCard
+                                                        key={index}
+                                                        {...project}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    Web
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    Mobile
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
+        </section>
+    )
+}
